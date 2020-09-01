e646a27879ad34b7fd5e4f0aa3bcdff51e06a9fd
cedric3c <cedric.carrere.dev@gmail.com>
2020-09-01 16:12:26 +0200

    modif ced

--- js/solitaire.js
+++ js/solitaire.js
@@ -9,8 +9,7 @@ let cardsReserve = [];
 let deck = [];
 let column = document.querySelector('#column');
 let hiddenFace = document.querySelector('#hiddenFace');
-let visibleFace = document.querySelector('#visibleFace')
-let img = document.querySelectorAll('#column img');
+let visibleFace = document.querySelector('#visibleFace');
 
 
 
@@ -59,6 +58,65 @@ function begin() {
     hiddenFace.append(img);
 }
 
+let coeur = [];
+let carreau = [];
+let trefle = [];
+let pique = [];
+
+
+doubleClic()
+function doubleClic(){
+    document.querySelectorAll('#column img, #visibleFace img').forEach(element => {
+        element.addEventListener('dblclick', ()=> {
+            let number = extraitNombre(element.getAttribute('src'))
+            console.log(number)
+            if (element.getAttribute('src').includes('coeur')){
+                if (coeur.length == number-1){
+                    element.style.marginTop = 0;
+                    document.querySelector('#coeur').append(element);
+                    coeur.push(number);
+                } 
+            } else if (element.getAttribute('src').includes('carreau')){
+                if (carreau.length == number-1){
+                    element.style.marginTop = 0;
+                    document.querySelector('#carreau').append(element);
+                    carreau.push(number)
+                } 
+            } else if (element.getAttribute('src').includes('trefle')){
+                if (trefle.length == number-1){
+                    element.style.marginTop = 0;
+                    document.querySelector('#trefle').append(element);
+                    trefle.push(number)
+                } 
+            } else if (element.getAttribute('src').includes('pique')){
+                if (pique.length == number-1){
+                    element.style.marginTop = 0;
+                    document.querySelector('#pique').append(element);
+                    pique.push(number)
+                } 
+            }
+        })
+    })
+}
+
+
+
+
+
+
+
+function extraitNombre(str){ return Number(str.replace(/[^\d]/g, "")) }
+ 
+
+
+
+
+
+
+
+
+
+
 // document.querySelector('#hiddenFace').addEventListener('click', () => {
 //     pickUp();
 // })
@@ -86,6 +144,7 @@ function begin() {
 //     if (cards.length == 0) {
 //         document.querySelector('#hiddenFace').childNodes[0].src = "image/fundation.png"
 //     }
+//     doubleClic()
 // }
 
 // // fonction de retournement de la pioche
@@ -99,158 +158,70 @@ function begin() {
 //     img.src = "image/back.png";
 //     document.querySelector('#hiddenFace').append(img);
 //     document.querySelector('#visibleFace').innerHTML = "";
-    // console.log(cards)
+//     console.log(cards)
 // }
 
 
 
-// let dropper = document.querySelectorAll('.dropper');
 
 
-// img.forEach(element => {
-//     element.addEventListener('dragstart', dragStart)
-//     element.addEventListener('dragend', dragEnd)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelectorAll('#column img').forEach(element => {
+//     element.addEventListener('dblclick', ()=> {
+//         let number = extraitNombre(element.getAttribute('src'))
+//         if (element.getAttribute('src').includes('coeur')){
+//             if (number == 1){
+//                 element.style.marginTop = 0;
+//                 document.querySelector('#coeur').append(element);
+//                 coeur.push(number)
+//             } else if (coeur.length == number-1){
+//                 element.style.marginTop = 0;
+//                 document.querySelector('#coeur').append(element);
+//                 coeur.push(number)
+//             } 
+//         } else if (element.getAttribute('src').includes('carreau')){
+//             if (number == 1){
+//                 element.style.marginTop = 0;
+//                 document.querySelector('#carreau').append(element);
+//                 carreau.push(number)
+//             } else if (carreau.length == number-1){
+//                 element.style.marginTop = 0;
+//                 document.querySelector('#carreau').append(element);
+//                 carreau.push(number)
+//             } 
+//         } else if (element.getAttribute('src').includes('trefle')){
+//             if (number == 1){
+//                 element.style.marginTop = 0;
+//                 document.querySelector('#trefle').append(element);
+//                 trefle.push(number)
+//             } else if (trefle.length == number-1){
+//                 element.style.marginTop = 0;
+//                 document.querySelector('#trefle').append(element);
+//                 trefle.push(number)
+//             } 
+//         } else if (element.getAttribute('src').includes('pique')){
+//             if (number == 1){
+//                 element.style.marginTop = 0;
+//                 document.querySelector('#pique').append(element);
+//                 pique.push(number)
+//             } else if (pique.length == number-1){
+//                 element.style.marginTop = 0;
+//                 document.querySelector('#pique').append(element);
+//                 pique.push(number)
+//             } 
+//         }
+//     })
 // })
-
-
-// function dragStart() {
-//     setTimeout(() => (this.className = 'invisible'), 0);
-// }
-
-// function dragEnd() {
-//     this.classList.remove("invisible")
-// }
-
-// for (const vide of dropper) {
-//     vide.addEventListener('dragover', dragOver)
-//     vide.addEventListener('dragenter', dragEnter)
-//     vide.addEventListener('dragleave', dragLeave)
-//     vide.addEventListener('drop', dragDrop)
-// }
-
-// function dragOver(e) {
-//     e.preventDefault();
-//     console.log('over')
-// }
-
-// function dragEnter(e) {
-//     e.preventDefault();
-//     console.log('enter')
-// }
-
-// function dragLeave() {
-//     console.log('leave')
-// }
-
-// function dragDrop() {
-//     console.log('drop')
-// }
-
-
-
-
-
-
-
-
-let cards = [];
-cards.forEach(element => {
-    let indice = Math.floor(Math.random() * Math.floor(cards.length))
-    let carteSorti = cards.splice(indice, 1);
-    return carteSorti;
-});
-
-//Distribution dans éléments
-function distribution() {
-    let deck=[];
-    let cardsPioche=[];
-
-    //au lancement de la partie, remplir la pioche
-    function remplirPioche() {
-        //Remplir de 24 cartes la pioche
-        for (let i = 0; i< 24; i++) {
-            cardsPioche.push(carteSorti);
-            Math.floor((Math.random().deck.length)+1);
-        };
-    };      
-    // Clic sur la pioche (deck[]);
-    hiddenFace.addEventListener('click', function(e){
-        // si l'emplacement pioche à son tableau vide,
-        if (deck.length>0){
-            deck=cardsPioche;// soucy, visual erreur : "message": "Une expression d'accès à un élément doit accepter un argument.",
-           deck.splice(carteSorti, 1);
-           cardsPioche.push(carteSorti);
-           visibleFace.prepend(carteSorti);
-        }else{
-           deck.splice(carteSorti, 1);
-           cardsPioche.push(carteSorti);
-           visibleFace.prepend(carteSorti);
-        }
-    });
-                
-    }
-
-
-
-
-
-
-
-
-
-
-let coeur = [];
-let carreau = [];
-let trefle = [];
-let pique = [];
-
-img.forEach(element => {
-    element.addEventListener('dblclick', ()=> {
-        let number = extraitNombre(element.getAttribute('src'))
-        if (element.getAttribute('src').includes('coeur')){
-            if (number == 1){
-                element.style.marginTop = 0;
-                document.querySelector('#coeur').append(element);
-                coeur.push(number)
-            } else if (coeur.length == number-1){
-                element.style.marginTop = 0;
-                document.querySelector('#coeur').append(element);
-                coeur.push(number)
-            } 
-        } else if (element.getAttribute('src').includes('carreau')){
-            if (number == 1){
-                element.style.marginTop = 0;
-                document.querySelector('#carreau').append(element);
-                carreau.push(number)
-            } else if (carreau.length == number-1){
-                element.style.marginTop = 0;
-                document.querySelector('#carreau').append(element);
-                carreau.push(number)
-            } 
-        } else if (element.getAttribute('src').includes('trefle')){
-            if (number == 1){
-                element.style.marginTop = 0;
-                document.querySelector('#trefle').append(element);
-                trefle.push(number)
-            } else if (trefle.length == number-1){
-                element.style.marginTop = 0;
-                document.querySelector('#trefle').append(element);
-                trefle.push(number)
-            } 
-        } else if (element.getAttribute('src').includes('pique')){
-            if (number == 1){
-                element.style.marginTop = 0;
-                document.querySelector('#pique').append(element);
-                pique.push(number)
-            } else if (pique.length == number-1){
-                element.style.marginTop = 0;
-                document.querySelector('#pique').append(element);
-                pique.push(number)
-            } 
-        }
-    })
-})
-
-
-function extraitNombre(str){ return Number(str.replace(/[^\d]/g, "")) }
- 
